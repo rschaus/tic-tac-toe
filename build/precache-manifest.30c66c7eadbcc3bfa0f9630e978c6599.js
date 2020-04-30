ea36b6bfff1ec0aad0d5c19bd5f655f2054fef8e
Reynaldo <reynaldo@MacBook-Pro-de-Reynaldo.local>
2020-04-30 21:29:46 +0200

    Updated files

---
+++ build/precache-manifest.30c66c7eadbcc3bfa0f9630e978c6599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405c7e0dd3477d471aab0896613479e9",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "98fca015113e2f5c4367",
+    "url": "/tic-tac-toe/static/css/main.72d41de6.chunk.css"
+  },
+  {
+    "revision": "a7a743f42dfe253da73c",
+    "url": "/tic-tac-toe/static/js/2.9314f0b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.9314f0b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98fca015113e2f5c4367",
+    "url": "/tic-tac-toe/static/js/main.54fc3b86.chunk.js"
+  },
+  {
+    "revision": "b83c3f5dfddd51f16016",
+    "url": "/tic-tac-toe/static/js/runtime-main.e9088102.js"
+  }
+]);
